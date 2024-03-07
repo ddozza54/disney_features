50536be1b97d4a1eb5745876f61008c9529ba21e
ddozza54 <bananamayong@gmail.com>
2024-03-08 00:32:36 +0900

    페이지, Header 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+
 function App() {
 
   return (
     <>
-
+      <Header />
+      App
+      <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import router from './router/Router'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
