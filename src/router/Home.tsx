31f8100b63e35f9f9245996bb3328f224a4c5a0b
ddozza54 <bananamayong@gmail.com>
2024-03-08 22:49:43 +0900

    캐릭터 data fetch

--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -1,7 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+
+interface Character {
+    id: number;
+    name: string;
+    imageUrl: string;
+}
 export default function Home() {
+    const fetchDisneyCharacters = async () =>
+        await fetch(`https://disney_api.nomadcoders.workers.dev/characters`).then(res => res.json()).then(data => data.slice(100));
+    const { isPending, isError, data, error } = useQuery<Character[]>({ queryKey: ['characters'], queryFn: fetchDisneyCharacters })
+
+    if (isPending) {
+        return <span>Loading...</span>
+    }
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+
+    console.log(data)
     return (
         <div>
             Home
+            Success!
+            {data.map(character => <div key={character.id}>
+                <img src={`${character.imageUrl}`} />
+                <h4>{character.name}</h4>
+            </div>)}
         </div>
     );
 }
