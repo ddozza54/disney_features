db5f82728ce352fccb7981aa39562a140651cfac
ddozza54 <bananamayong@gmail.com>
2024-03-09 00:53:59 +0900

    Header 생성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/disneyLogo.png'
+
 export default function Header() {
     return (
         <div>
-            Header
+            <Link to="/">
+                <img src={logo} alt='Disney' />
+            </Link>
         </div>
     );
 }
