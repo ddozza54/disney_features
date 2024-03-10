09e77f00767f25c1f72be9833c93181c6ea3ac6d
ddozza54 <bananamayong@gmail.com>
2024-03-11 02:06:15 +0900

    Style: Header margin-bottom 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import logo from '../assets/disneyLogo.png'
 
 export default function Header() {
     return (
-        <div className='w-full flex justify-center mb-[30rem] xl:mb-[35rem]'>
+        <div className='w-full flex justify-center mb-[24rem] xl:mb-[29rem]'>
             <Link to="/">
-                <div className='w-48 h-32 p-16 rounded-full hover:animate-ping hover:bg-white xl:w-56 xl:h-40'></div>
+                <div className='w-48 h-32 p-16 rounded-full hover:animate-ping hover:bg-white xl:w-56 xl:h-4'></div>
             </Link>
         </div>
     );
